bb70febfb94c63e63a1a11df158f69fe3dabee52
John Bogart <johnjackbogart@gmail.com>
2025-12-21 19:56:07 -0500

    enter insert mode when opening opencode

--- lua/johnjackbogart/plugins/opencodesudo.lua
+++ lua/johnjackbogart/plugins/opencodesudo.lua
@@ -4,6 +4,24 @@ return {
 	enabled = true,
 	config = function()
 		require("opencode").setup({})
+
+		local opencode_insert_group = vim.api.nvim_create_augroup("opencode_insert", { clear = true })
+
+		vim.api.nvim_create_autocmd("FileType", {
+			group = opencode_insert_group,
+			pattern = { "opencode", "opencode_input", "opencode_prompt" },
+			callback = function(args)
+				vim.schedule(function()
+					if not vim.api.nvim_buf_is_valid(args.buf) then
+						return
+					end
+					if vim.api.nvim_get_current_buf() ~= args.buf then
+						return
+					end
+					vim.cmd("startinsert")
+				end)
+			end,
+		})
 	end,
 	dependencies = {
 		"nvim-lua/plenary.nvim",
