a138a3d64dbde2a9eaef0ad3345baa3b1d1a36a8
jack bogart <johnjackbogart@gmail.com>
2026-08-16 17:07:41 -0400

    update telescopre

--- lua/johnjackbogart/plugins/telescope.lua
+++ lua/johnjackbogart/plugins/telescope.lua
@@ -1,13 +1,14 @@
 return {
-  { 
-    "nvim-telescope/telescope-fzf-native.nvim",
-    build = "make" 
-  },
-  { 
-    "nvim-telescope/telescope.nvim", tag = "0.1.5",
-    dependencies = { "nvim-lua/plenary.nvim" },
-    config = function()
-      require("telescope").load_extension("fzf")
-    end
-  }
+	{
+		"nvim-telescope/telescope-fzf-native.nvim",
+		build = "make",
+	},
+	{
+		"nvim-telescope/telescope.nvim",
+		version = "*",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		config = function()
+			require("telescope").load_extension("fzf")
+		end,
+	},
 }
